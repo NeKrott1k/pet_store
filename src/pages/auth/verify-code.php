83b5a229a840c1c76c98502d514e4140a2ebd02e
TimGizz <timgizzat@gmail.com>
2026-05-10 23:47:23 +0400

    Реализована серверная сторона регистрации и авторизации с подтверждением email (#2)
    * Реализована регистрация и авторизация с подтверждением email
    
    * вынес чувствительные данные в .env
    
    * отключил код подтверждения для admin и manager при авторизации
    
    ---------
    
    Co-authored-by: Timur <timgizzat@gmail>

---
+++ src/pages/auth/verify-code.php
@@ -0,0 +1,22 @@
+<?php
+$from = $_SESSION["verify_from"] ?? '';
+
+if(!in_array($from, ["register", "login"])){
+    header("Location: /");
+    exit;
+}
+
+
+$input_error = $_SESSION["error"] ?? [];
+
+unset($_SESSION["error"]);
+?>
+<form action="/<?= $from ?>" method="POST">
+    <label for="">Введите код.</label>
+    <label for="">код отправлен на почту</label>
+    <input type="text" name="verify_code">
+    <?php if (isset($input_error["verify_code"])): ?>
+        <div style="color: red;"><?= $input_error["verify_code"] ?></div>
+    <?php endif ?>
+    <button type="submit">Submit</button>
+</form>
